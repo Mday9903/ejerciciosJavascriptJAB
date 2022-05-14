256ff3277ac3799fe0b1b7dc34c0ba5ca9d10a39
Marcos Day <marcosday90@gmail.com>
2022-05-13 23:04:26 -0300

    Finalizamos el ejercicio 1

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,3 +1,6 @@
+//Tenemos cuatro botones y les vamos a asignar distintas funciones a cada uno.
+
+//Queremos que la primera función que ejecute apenas se cargue la página sea Inicio.
 window.onload=inicio;
 
 
@@ -9,19 +12,21 @@ function inicio(){
    
 }
 
+//El primer botón escribe en el div "Hola Mundo".
 function accion1(){
-    document.querySelector("#text-box").innerHTML="Hola Mundo";
+    document.querySelector("#box").innerHTML="Hola Mundo";
 }
 
+//El segundo botón inserta una imágen en el div.
 function accion2(){
-    document.querySelector("#text-box").innerHTML="<img src='./ejercicio1/html5.png'>";
+    document.querySelector("#box").innerHTML="<img src='./ejercicio1/html5.png'>";
 }
-
+//El tercer botón inserta otro botón a continuación del contenido dentro del div.
 function accion3(){
-    document.querySelector("#text-box").insertAdjacentHTML("beforeend","<button>Otro botòn</button>");
+    document.querySelector("#box").insertAdjacentHTML("beforeend","<button>Otro botòn</button>");
 }
-
+//El cuarto botón vacía el div.
 function accion4(){
-    document.querySelector("#text-box").innerHTML="";
+    document.querySelector("#box").innerHTML="";
 }
 
